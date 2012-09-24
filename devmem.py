e3e76e2516bdb722cdb70115d62129f29d27ef8e
Andrea Righi <andrea@betterlinux.com>
2012-09-24 20:50:39 +0200

    add a user-space tool to read data from /dev/rawmem
    Signed-off-by: Andrea Righi <andrea@betterlinux.com>

---
+++ devmem.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 021110-1307, USA.
+#
+# Copyright (C) 2012 Andrea Righi <andrea@betterlinux.com>
+
+import sys, os, mmap
+
+PAGE_MASK = mmap.PAGESIZE - 1
+
+def hexdump(num):
+    return '' . join([ "%02x " % ord(x) for x in num]).strip()
+
+def main():
+    # parse cmdline
+    if len(sys.argv) != 4:
+        print "Usage: %s DEVICE ADDRESS SIZE" % sys.argv[0]
+        sys.exit(1)
+    dev = sys.argv[1]
+    addr = int(sys.argv[2], 0)
+    size = int(sys.argv[3], 0)
+
+    # do mmap()
+    fd = os.open(dev, os.O_RDONLY | os.O_SYNC)
+    m = mmap.mmap(fd, mmap.PAGESIZE, mmap.MAP_SHARED, mmap.PROT_READ,
+                    offset=addr & ~PAGE_MASK)
+    m.seek(addr & PAGE_MASK)
+    c = m.read(size)
+    m.close()
+    os.close(fd)
+
+    print hexdump(c)
+
+if __name__ == '__main__':
+    main()
